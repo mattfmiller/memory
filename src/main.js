8538338a0dbc4c36f779a65bccb721d8f2f326a9
Cory West-Forbes and Matt Miller <cory@>
2018-04-16 15:17:56 -0700

    add images to dist folder and func to populate images

--- src/main.js
+++ src/main.js
@@ -1,1 +1,30 @@
+import {Board} from './board.js';
 import './styles.css';
+
+function assignValues(obj) {
+  for (var i = 0; i < obj.slots.length; i++) {
+    $('#' + (i + 1)).attr('name', obj.slots[i].type);
+    console.log('td #' + (i + 1).toString());
+  }
+}
+
+function assignImgs(obj) {
+  for (var i = 0; i < obj.slots.length; i++) {
+    $('#' + (i + 1)).append('<img src="' + obj.slots[i].type + '.png">');
+  }
+}
+
+$(function (){
+  var gameBoard = new Board();
+  gameBoard.createDeck();
+  gameBoard.drawBoard();
+  console.log(gameBoard);
+  assignValues(gameBoard);
+  assignImgs(gameBoard);
+
+  $('.face-card').click(function(){
+    var clickedCard = $(this).attr('name');
+    console.log(clickedCard);
+  });
+
+});
--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: "eslint-loader"
-      }
+      },
     ]
   }
 };
